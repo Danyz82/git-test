f5286c752a6f0ad90e4135108f521928b26d14cd
Daniel Blach <daniel.blach@intive.com>
2020-02-17 11:00:13 +0100

    Commit to squash on top

--- two.js
+++ two.js
@@ -1,2 +1,4 @@
-const two_1 = "text two 1";
-const two_2 = "text two 2";
+const two_1 = "text two 1 commit-squash-1 1";
+const two_2 = "text two 2 commit-squash-2 2";
+const two_3 = "text two 3 commit-squash-3 3";
+const two_4 = "text two 4 commit-squash-4 4";
