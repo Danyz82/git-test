539c14185f92ec8b3df80319a524d62534a29cdc
Daniel Blach <daniel.blach@intive.com>
2020-02-17 11:12:24 +0100

    Ammend test

--- one.js
+++ one.js
@@ -1,3 +1,3 @@
-const one_1 = "text one 1 changed my local changes 1";
-const one_3 = "text one 3 3";
-const one_2 = "text one 2 changed my local changes 2";
+const one_1 = "text one 1 changed my local changes 1 1";
+const one_3 = "text one 3 3 3";
+const one_2 = "text one 2 changed my local changes 2 2";
