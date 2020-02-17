014bc0b4a048d7d77d2f1fd372185b5564c1a51e
Daniel Blach <32126772+Danyz82@users.noreply.github.com>
2020-02-17 10:47:05 +0100

    Update one.js

--- one.js
+++ one.js
@@ -1,3 +1,3 @@
-const one_1 = "text one 1 changed";
+const one_1 = "text one 1 changed on master";
 const one_3 = "text one 3";
-const one_2 = "text one 2 changed";
+const one_2 = "text one 2 changed on master";
