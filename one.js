6d9a1dfd9ff7f5a83bc77d1954762c1b93a79fdc
Daniel Blach <daniel.blach@intive.com>
2020-02-17 10:46:48 +0100

    Third commit

--- one.js
+++ one.js
@@ -1,3 +1,3 @@
-const one_1 = "text one 1 changed on master";
+const one_1 = "text one 1 changed my local changes";
 const one_3 = "text one 3";
-const one_2 = "text one 2 changed on master";
+const one_2 = "text one 2 changed my local changes";
