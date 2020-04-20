7a6c4b00adb131ec3b47230af15d3681aaf4f66f
Daniel Blach <daniel.blach@intive.com>
2020-04-20 07:25:01 +0200

    Cherry pick commit 2

--- two.js
+++ two.js
@@ -3,3 +3,4 @@ const two_2 = "text two 2 commit-squash-2 2";
 const two_3 = "text two 3 commit-squash-3 3";
 const two_4 = "text two 4 commit-squash-4 4";
 const three = 'tak';
+const four = 'four';
